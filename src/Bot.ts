f26990eafbb476983ffd801a18c8bb865184fb9a
Fabian H <fab2001hell@gmail.com>
2022-02-07 21:08:25 +0100

    Created PI2 bot

---
+++ src/Bot.ts
@@ -0,0 +1,18 @@
+import { Client } from "discord.js";
+import { readFileSync } from "fs";
+
+import ready from "./listeners/ready";
+import interactionCreate from "./listeners/interactionCreate";
+
+
+const config = JSON.parse(readFileSync("./local-config.json", "utf8"));
+const token = config.token;
+
+console.log("Bot is starting");
+
+const client = new Client ({intents: []});
+ready(client);
+interactionCreate(client);
+client.login(token);
+
+console.log(client);
